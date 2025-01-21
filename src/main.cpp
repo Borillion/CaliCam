05f85b766c8df41004e2a4b2291f72102c9ba904
Borillion <borillion_star@yahoo.com>
2025-01-20 21:25:36 -0800

    Modify license with GNU Lesser General Public License v2.1, start implementing code for Camera streaming server

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 // put function declarations here:
-#ifndef LED_BUILTIN
-    #define LED_BUILTIN 2
-#endif
 
 void setup() {
   // put your setup code here, to run once:
+
+  // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  const char *ssid = "**********";
+  const char *password = "**********";
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    while (WiFi.status() == WL_CONNECTED) {
+      digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+      delay(500);             // wait for 500 milliseconds
+      digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
+      delay(500);             // wait for 500 milliseconds
+    }
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
-  delay(1000);
+  // Do nothing. Everything is done in another task by the web server
+  delay(10000);
 }
