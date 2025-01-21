d48ae1f426f410df6619e245ad8d0eeac4c7e07a
Borillion <borillion_star@yahoo.com>
2025-01-20 20:20:38 -0800

    Add Blink test code for pio, license, and README

---
+++ README.md
@@ -0,0 +1,3 @@
+This is dev code for Calicam on a Seeed Studio Xiao ESP32S3 Sense dev board.
+
+Please don't do evil! 
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+#ifndef LED_BUILTIN
+    #define LED_BUILTIN 2
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(1000);
+}
