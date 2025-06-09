f93b1ff9050366c20cfb45dc9bd2e9bd03779dc3
Borillion <borillion_star@yahoo.com>
2025-06-08 18:54:46 -0700

    Ensure wifi_config.h is ignored

--- include/wifi_config.h
+++ include/wifi_config.h
@@ -2,7 +2,7 @@
 #define WIFI_CONFIG_H
 
 // Replace with your WiFi credentials
-const char* WIFI_SSID = "your-ssid";
-const char* WIFI_PASS = "your-password";
+const char* WIFI_SSID = "BabakFold";
+const char* WIFI_PASS = "Eriamjh@3441";
 
 #endif // WIFI_CONFIG_H
